8100c90f903f40b7fc70f38c00a765ed49a79402
anjalivs990 <anjalivs990@gmail.com>
2025-12-13 22:25:59 +0530

    fix: set from address for Brevo SMTP

--- ECommerce/src/main/java/com/Anjali/ECommerce/Service/EmailService.java
+++ ECommerce/src/main/java/com/Anjali/ECommerce/Service/EmailService.java
@@ -1,33 +1,48 @@
 package com.Anjali.ECommerce.Service;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+
 @Service
 @RequiredArgsConstructor
 public class EmailService {
+
     private final JavaMailSender javaMailSender;
 
-    public void sendVerificationOtpEmail(String userEmail, String otp, String subject, String text) throws MessagingException {
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    public void sendVerificationOtpEmail(
+            String userEmail,
+            String otp,
+            String subject,
+            String text
+    ) throws MessagingException {
+
         try {
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "UTF-8");
+            MimeMessageHelper helper
+                    = new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_MIXED, "UTF-8");
 
-            mimeMessageHelper.setSubject(subject);
-            mimeMessageHelper.setText(text);
-            mimeMessageHelper.setTo(userEmail);
+            helper.setFrom(fromEmail);
+            helper.setTo(userEmail);
+            helper.setSubject(subject);
+
+            helper.setText(text, false);
 
             javaMailSender.send(mimeMessage);
-        }
-        catch (MailException e) {
-            throw new MailSendException("Failed to send Email");
+
+        } catch (MailException e) {
+            throw new MailSendException("Failed to send Email", e);
         }
     }
 }
